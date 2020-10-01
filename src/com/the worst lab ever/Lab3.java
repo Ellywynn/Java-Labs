649f66ad5c8ebdddf923bdcc3b9827dc2f30c6cd
Ilya <57060144+Ellywynn@users.noreply.github.com>
2020-10-01 21:12:46 +0300

    updated lab3

--- src/com/the worst lab ever/Lab3.java
+++ src/com/the worst lab ever/Lab3.java
@@ -3,28 +3,42 @@ package com.Lab;
 import java.util.Scanner;
 
 public class Lab3 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.printf("Введите размер матрицы: ");
-        int size = input.nextInt();
-
-        int matrix[][] = new int[size][size];
+    static void printMatrix(int m[][]){
+        int size = m.length;
         for(int i=0; i<size; i++){
             for(int j=0; j<size; j++){
-                matrix[i][j] = (int)(Math.random() * 10);
-
-                if(i==j) {
+                if(i == size/2 && j == size/2){
+                    System.out.printf("X ");
+                }
+                else if(i==j) {
                     System.out.printf("\\ ");
                 }else if(i==size-1-j){
                     System.out.printf("/ ");
                 }else {
-                    System.out.printf("%d ", matrix[i][j]);
+                    System.out.printf("%d ", m[i][j]);
                 }
-
             }
             System.out.printf("\n");
         }
+    }
 
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.printf("Введите размер матрицы: ");
+        // ввод размера матрицы
+        int size = input.nextInt();
+
+        // создание матрицы, инициализация случайными числами [0;9]
+        int matrix[][] = new int[size][size];
+        for(int i=0; i<size; i++){
+            for(int j=0; j<size; j++){
+                matrix[i][j] = (int)(Math.random() * 10);
+            }
+        }
+
+        printMatrix(matrix);
+
+        // перестановка центральных элементов по горизонтальной оси, если размер матрицы нечетный
         if(size%2!=0){
             int j = size/2;
             for(int i = 0; i < size/2; i++){
@@ -34,32 +48,24 @@ public class Lab3 {
             }
         }
 
+        // перестановка элементов матрицы
         for(int i=0; i<size/2; i++){
             for(int j=0; j<size; j++){
+                // если элемент принадлежит главной или обратной диагоналям - скип
                 if(i == j || i == size-j-1){
                     continue;
                 }
                 else{
-                        int temp = matrix[i][j];
-                        matrix[i][j] = matrix[size - i - 1][size - j - 1];
-                        matrix[size - i - 1][size - j - 1] = temp;
+                    // заменяем элементы крест накрест
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[size - i - 1][size - j - 1];
+                    matrix[size - i - 1][size - j - 1] = temp;
                 }
             }
         }
 
         System.out.println("\nПосле отражения:\n");
 
-        for(int i=0; i<size; i++){
-            for(int j=0; j<size; j++){
-                if(i==j) {
-                    System.out.printf("\\ ");
-                }else if(i==size-1-j){
-                    System.out.printf("/ ");
-                }else {
-                    System.out.printf("%d ", matrix[i][j]);
-                }
-            }
-            System.out.printf("\n");
-        }
+        printMatrix(matrix);
     }
 }
